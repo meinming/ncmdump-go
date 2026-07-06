c246d9b067bb5d727132e36de699d22e9cc0223f
Meinming <wxm5896@outlook.com>
2026-07-07 03:09:56 +0800

    refactor: optimize RC4 initialization and decryption logic for clarity

--- pkg/crypto/rc4.go
+++ pkg/crypto/rc4.go
@@ -23,18 +23,25 @@ func NewNCMRC4(key []byte) *NCMRC4 {
 	// 2. 【标准 RC4 初始化】：利用密钥打乱 S 盒
 	j := 0
 	keyLen := len(key)
-	for i := 0; i < 256; i++ {
-		j = (j + int(sBox[i]) + int(key[i%keyLen])) & 0xFF
-		sBox[i], sBox[j] = sBox[j], sBox[i]
+	for i := range 256 {
+		// j = (j + int(sBox[i]) + int(key[i%keyLen])) & 0xFF
+		// sBox[i], sBox[j] = sBox[j], sBox[i]
+		jq := uint8(j) + sBox[i] + key[i%keyLen]
+		sBox[i], sBox[jq] = sBox[jq], sBox[i]
+
 	}
 
 	// 3. 【网易云变种算法】：生成静态密钥字典
 	// 修复点：这里的 idx 计算是完全独立的，千万不能继承上一个循环的 j
 	for i := 0; i < 256; i++ {
-		idx := (i + 1) & 0xFF
-		s_j := int(sBox[idx])
-		s_jj := int(sBox[(s_j+idx)&0xFF])
-		ncm.keyBox[i] = sBox[(s_jj+s_j)&0xFF]
+		// idx := (i + 1) & 0xFF
+		// s_j := int(sBox[idx])
+		// s_jj := int(sBox[(s_j+idx)&0xFF])
+		// ncm.keyBox[i] = sBox[(s_jj+s_j)&0xFF]
+		idx := uint8(i + 1)
+		s_j := sBox[idx]
+		s_jj := sBox[s_j+idx]
+		ncm.keyBox[i] = sBox[s_jj+s_j]
 	}
 
 	return ncm
@@ -42,10 +49,11 @@ func NewNCMRC4(key []byte) *NCMRC4 {
 
 // Decrypt 执行流解密（原地修改）
 func (n *NCMRC4) Decrypt(data []byte) {
-	for i := 0; i < len(data); i++ {
+	for i := range data {
 		// 异或解密
 		data[i] ^= n.keyBox[n.keyPos]
 		// 修复点：利用位运算代替 python 中的 if/else 判断，实现完美的 256 环形取模
-		n.keyPos = (n.keyPos + 1) & 0xFF
+		// n.keyPos = (n.keyPos + 1) & 0xFF
+		n.keyPos = n.keyPos + 1
 	}
 }
